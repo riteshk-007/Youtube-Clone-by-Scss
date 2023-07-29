88b9e832a14680ab5b637ab109b8617c0727303b
Ritesh <codeshorts007@gmail.com>
2023-07-29 16:28:08 +0530

    search page

--- src/components/SeachResultVideoCard/SeachResultVideoCard.jsx
+++ src/components/SeachResultVideoCard/SeachResultVideoCard.jsx
@@ -1,7 +1,37 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import "./SeachResultVideoCard.scss";
+import { abbreviateNumber } from "js-abbreviation-number";
+
+import VideoLength from "../../video/VideoLength";
 import "./SeachResultVideoCard.scss";
 
-function SeachResultVideoCard() {
-  return <></>;
+function SeachResultVideoCard({ video }) {
+  return (
+    <Link to={`/video/${video?.videoId}`}>
+      <div className="video-card1">
+        <span className="position1">
+          <img className="video-card-img1" src={video?.thumbnails[0]?.url} />
+          {video?.lengthSeconds && <VideoLength time={video?.lengthSeconds} />}
+        </span>
+        <div className="flex-box1">
+          <span className="flex-box-title1">{video?.title}</span>
+          <span className="title-chennel-name1">{video?.author?.title}</span>
+          <span>
+            <span className="title-chennel-name1">{`${abbreviateNumber(
+              video?.stats?.views,
+              2
+            )} views`}</span>
+          </span>
+          <span>
+            <span className="title-chennel-name1">
+              {video?.publishedTimeText}
+            </span>
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
 export default SeachResultVideoCard;
--- src/components/searchResults/SearchResult.jsx
+++ src/components/searchResults/SearchResult.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import "./SearchResult.scss";
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "../../Context/ContextApi";
+import { FetchApiData } from "../../utils/api";
+import LaftNav from "../LeftNev/LeftNav";
+import SearchResultVideoCard from "../SeachResultVideoCard/SeachResultVideoCard";
 function SearchResult() {
-  return <div></div>;
+  const { setLoading } = useContext(Context);
+  const [result, setResult] = useState();
+  const { searchQuery } = useParams();
+
+  useEffect(() => {
+    document.getElementById("root").classList.remove("custom-h");
+    fetchSearchResult();
+  }, [searchQuery]);
+
+  const fetchSearchResult = () => {
+    setLoading(true);
+    FetchApiData(`search/?q=${searchQuery}`).then((res) => {
+      setResult(res.contents);
+      setLoading(false);
+    });
+  };
+
+  return (
+    <div className="search-page">
+      <LaftNav />
+      <div className="serach-container">
+        {result?.map((item, index) => {
+          if (item.type !== "video") return false;
+          return <SearchResultVideoCard key={index} video={item?.video} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default SearchResult;
